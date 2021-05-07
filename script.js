25c1340837c28e815627f5c3e74a476f2291c435
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-05-07 11:56:26 +0530

    Added logic to genrate password

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  if (typesCount === 0) return '';
+  // Object.values(item) covert object's key value pairs to arra of values
+  const typesArr = [{ lower }, { upper }, { symbol }, { number }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  return generatedPassword.slice(0, length);
+}
 
 function getRandomLower() {
   // Generates a random number > than 97
